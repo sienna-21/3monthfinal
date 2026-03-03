8d6ee45e88a1c0bb2640206131f54af3e8dbfc25
sienna-21 <kailovesyou21@gmail.com>
2026-03-04 04:06:15 +0530

    Add vintage bg image

--- src/components/EnvelopePage.tsx
+++ src/components/EnvelopePage.tsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState, useMemo } from "react";
-import vintagePaper from "../assets/vintage-bg.jpg";
+import vintageBg from "../assets/vintage-bg.jpg";
 interface EnvelopePageProps {
   onComplete: () => void;
 }
